64ce803b4564ad9588dcb42ee26f2a9afe417438
pester33 <130249635+pester33@users.noreply.github.com>
2026-08-13 21:59:04 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 
+var DOWNLOAD_URL = "#";
+
+var dl = document.getElementById("download-link");
+dl.setAttribute("href", DOWNLOAD_URL);
+dl.addEventListener("click", function (e) {
+  if (DOWNLOAD_URL === "#") {
+    e.preventDefault();
+    alert("Download coming soon!");
+  }
+});
